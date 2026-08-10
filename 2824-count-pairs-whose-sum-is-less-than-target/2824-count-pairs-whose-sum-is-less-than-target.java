369511f2afc4e1ffe3a555609864f2433ddcf76b
Vishok <p.vishok969@gmail.com>
2026-08-10 09:53:21 +0530

    Time: 2 ms (97.7%), Space: 44.1 MB (48.8%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -3,10 +3,8 @@ class Solution {
         int n=arr.size();
         int c=0;
         for(int i=0;i<n;i++){
-            int s=0;
             for(int j=i+1;j<n;j++){
-                s=arr.get(i)+arr.get(j);
-                if(s<t){
+                if(arr.get(i)+arr.get(j)<t){
                     c++;
                 }
             }
